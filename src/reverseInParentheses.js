bbbdf32b38ab22c18d6355364c02806be5190822
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 14:39:20 -0300

    update reverseInParentheses.js

--- src/reverseInParentheses.js
+++ src/reverseInParentheses.js
@@ -21,3 +21,31 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test reverseInParentheses
+
+// alternative solution
+// function solution(inputString) {
+//     while (inputString.includes('(')) {
+//         inputString = inputString.replace(/\(([^()]*)\)/, (_, str) => [...str].reverse().join(''));
+//     }
+//     return inputString;
+// }
+
+// alternative solution
+// function solution(s) {
+//     while (true) {
+//         let c = s.indexOf(')');
+//         if (c === -1) {
+//             break;
+//         }
+//         let o = s.substring(0, c).lastIndexOf('(');
+//         let start = s.substring(0, o);
+//         let middle = s
+//             .substring(o + 1, c)
+//             .split('')
+//             .reverse()
+//             .join('');
+//         let end = s.substring(c + 1, s.length);
+//         s = start + middle + end;
+//     }
+//     return s;
+// }
